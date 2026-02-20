c6c45313e295380b51b682f1ddc135473c43be20
cuwan <envatoprivate39@gmail.com>
2026-02-20 09:37:59 +0700

    Setup auto-deploy GitHub Actions & Fix Base URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Memastikan path relatif agar aman di GitHub Pages/Vercel
+  base: '/triallab/', // Sesuaikan dengan nama repository di GitHub
 })
